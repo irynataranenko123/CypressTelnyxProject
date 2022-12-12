f54cab4e6d32bf51e3ea0d2ac98479546a30b760
PepesSpi123 <irena.tv133@gmail.com>
2022-12-12 10:10:40 +0200

    Change data

--- cypress/src/utils/const.js
+++ cypress/src/utils/const.js
@@ -1,9 +1,9 @@
 import { generateUsername } from "unique-username-generator";
 
 export const email = generateUsername() +'@gmail.com'
-export const password = 'Qwerty12345))'
+export const password = 'Qwerty123456))'
 export const fullName = 'Full Name'
-export const validEmail = 'zukor1@company.com'
+export const validEmail = 'zukor3@gmail.com'
 export const notBusinessEmail = 'zukor@mail.ru'
 export const invalidEmail = 'rgmegaeeg@gmail.com'
 export const text = 'Telnyx'
