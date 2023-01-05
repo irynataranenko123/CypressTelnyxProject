8677cf4a44b82b163044b615e98c492947a6c88c
PepesSpi123 <119141458+PepesSpi123@users.noreply.github.com>
2023-01-05 20:25:17 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,17 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
-  reporter: "cypress-multi-reporters",
-  reporterOptions: {
-    configFile: "reporter-config.json"
-  },
-
   chromeWebSecurity: false,
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 60000,
   retries	: { "runMode": 2, "openMode": 2 },
   screenshot: 'on',
   //video: false,
+  env: {
+    allureResultsPath: '../allure-results',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
