d02a56807d14e4a8926a623be447093b20e1fb55
PepesSpi123 <irena.tv133@gmail.com>
2023-01-06 15:03:36 +0200

    Fixed tests

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,11 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
 
   chromeWebSecurity: false,
-  defaultCommandTimeout: 10000,
-  pageLoadTimeout: 60000,
+  //defaultCommandTimeout: 10000,
+  //pageLoadTimeout: 60000,
   retries	: { "runMode": 2, "openMode": 2 },
   screenshot: 'on',
-  video: false,
+  //video: false,
   screenshotsFolder: 'reports/screenshots',
   videosFolder: 'reports/videos',
   allureReuseAfterSpec: true,
